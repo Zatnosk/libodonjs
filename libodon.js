70f1603cdb68ade14bfca89ba6fe6fcb9de772c6
Jonas Ramsdal Ernstsen <zatnosk@zatnosk.dk>
2016-11-17 21:44:31 +0100

    Added function calls for each API endpoint
    There has only been limited testing of timeline endpoints - the rest is
    currently untestet.

--- libodon.js
+++ libodon.js
@@ -6,17 +6,135 @@
 		}
 
 		connect(server, username){
+			let connection_resolve, connection_reject;
+			connections.push(new Promise((resolve,reject)=>{
+				connection_resolve=resolve
+				connection_reject=reject
+			}))
+			active_connection = connections.length-1
 			return get_registration(server, this.appname, this.redirect_url)
 			.then(reg=>get_token(server, reg))
-			.then(token=>({result:'token',token:token}),
-				  error=>({result:'redirect',target:get_authorization_url(server, reg)}))
+			.then(
+			token=>{
+				connection_resolve({server:server,token:token})
+				return {result:'success'}
+			},
+			 error=>{
+			 	active_connection = undefined
+			 	connection_reject({error:'failed connection'})
+			 	return {result:'redirect',target:get_authorization_url(server, reg)}
+			})
 		}
+
+		timeline(target, options){
+			let endpoint = ''
+			switch(target){
+				case 'home': endpoint = '/api/v1/timelines/home'; break
+				case 'mentions': endpoint = '/api/v1/timelines/mentions'; break
+				case 'public': endpoint = '/api/v1/timelines/public'; break
+				default:
+					if(target.substring(0,1)=='#')
+						endpoint = '/api/v1/timelines/tag/'+target.substring(1)
+					break
+			}
+			if(endpoint=='') return Promise.reject('invalid timeline target')
+			return get_request(endpoint+timeline_options(options))
+		}
+
+		status(id){return get_request('/api/v1/statuses/'+id)}
+		account(id){return get_request('/api/v1/accounts/'+id)}
+		account_self(){return get_request('/api/v1/accounts/verify_credentials')}
+		account_statuses(id,options){
+			return get_request('/api/v1/accounts/'+id+'/statuses'+timeline_options(options))
+		}
+		followers(id){return get_request('/api/v1/accounts/'+id+'/followers')}
+		relationships(...ids){
+			if(!ids.length) return Promise.reject('no id given')
+			let query_parameters = '?'
+			if(ids.length == 1) query_parameters += 'id='+ids[0]
+			else query_parameters += 'id[]='+ids.join('&id[]=')
+			return get_request('/api/v1/accounts/relationships'+query_parameters)
+		}
+		suggestions(){return get_request('/api/v1/accounts/suggestions')}
+		context(id){return get_request('/api/v1/statuses/'+id+'/context')}
+		reblogged_by(id){return get_request('/api/v1/statuses/'+id+'/reblogged_by')}
+		favourited_by(id){return get_request('/api/v1/statuses/'+id+'/favourited_by')}
+
+		follow_remote(url){return post_request('/api/v1/follows',{uri:url})}
+		reblog(id){return post_request('/api/v1/statuses/'+id+'/reblog')}
+		unreblog(id){return post_request('/api/v1/statuses/'+id+'/unreblog')}
+		favourite(id){return post_request('/api/v1/statuses/'+id+'/favourite')}
+		unfavourite(id){return post_request('/api/v1/statuses/'+id+'/unfavourite')}
+		follow(id){return post_request('/api/v1/accounts/'+id+'/follow')}
+		unfollow(id){return post_request('/api/v1/accounts/'+id+'/unfollow')}
+		block(id){return post_request('/api/v1/accounts/'+id+'/block')}
+		unblock(id){return post_request('/api/v1/accounts/'+id+'/unblock')}
+
 	}
 	this.Libodon = Libodon
+
+	const connections = []
+	let active_connection = undefined;
 
 	const prefix = 'libodon'
 	const log_errors = true
 	const log_actions = true
+
+	function timeline_options(options){
+		if(typeof options == 'object'){
+			const params = []
+			if(options.max_id) params.push('max_id='+options.max_id)
+			if(options.since_id) params.push('since_id='+options.since_id)
+			if(options.limit) params.push('limit='+options.limit)
+			if(params.length) return '?'+params.join('&')
+		}
+		return ''
+	}
+
+	function get_request(endpoint){
+		if(connections.length == 0
+			|| typeof active_connection=='undefined'
+			|| typeof connections[active_connection]=='undefined'){
+			return Promise.reject('not connected')
+		}
+		return connections[active_connection].then(conn=>{
+			if(conn.error) return Promise.reject('not connected')
+			const server = conn.server;
+			const token = conn.token.access_token;
+			const fetchHeaders = new Headers();
+			fetchHeaders.set('Authorization','Bearer '+token);
+			const fetchInit = {
+				method:'GET',
+				mode:'cors',
+				headers: fetchHeaders
+			}
+			return fetch(server+endpoint, fetchInit).then(res=>res.json());
+		})
+	}
+
+	function post_request(endpoint,data){
+		if(connections.length == 0
+			|| typeof active_connection=='undefined'
+			|| typeof connections[active_connection]=='undefined'){
+			return Promise.reject('not connected')
+		}
+		return connections[active_connection].then(conn=>{
+			if(conn.error) return Promise.reject('not connected')
+			const server = conn.server;
+			const token = conn.token.access_token;
+			const fetchHeaders = new Headers();
+			fetchHeaders.set('Authorization','Bearer '+token);
+			const body = new URLSearchParams()
+			for(var key in data) body.set(key,data[key])
+			const fetchInit = {
+				method:'POST',
+				mode:'cors',
+				headers: fetchHeaders,
+				body: body
+			}
+			return fetch(server+endpoint, fetchInit).then(res=>res.json());
+		})
+	}
 
 	function get_token(server, registration){
 		const token = localStorage.getItem(prefix+'_token')
